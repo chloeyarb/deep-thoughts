2a67a3d6c6e0b0bdbf924e821d0a249416ccc3b5
Chloe Yarborough <cmyarboro@gmail.com>
2022-05-19 07:36:50 -0400

    added mutations

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
@@ -8,7 +9,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs, 
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
